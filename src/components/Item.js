9527867b83d9883fc1afd11aed0e569f88f6fc5f
tonniegeetz <tonniegeetz@gmail.com>
2024-05-25 01:31:54 +0300

    worked on state and events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false)
+
+  function changeCart() {
+    setInCart((empty) => !empty)
+  }
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={changeCart} className="add">
+        {inCart ? "Remove from cart" : "Add to cart"}
+      </button>
     </li>
   );
 }
